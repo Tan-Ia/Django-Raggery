90024e26d9a3c734673e6142434a6d021ac2de2f
kopika <taniafci81@gmail.com>
2019-04-16 11:02:51 +0600

    change subcategory

--- Raggery/Backend/forms/subcategory_form.py
+++ Raggery/Backend/forms/subcategory_form.py
@@ -7,10 +7,10 @@ class SubcategoryForm(forms.ModelForm):
         fields='__all__'
         category_choices=CategoryModel.objects.filter(category_status="Active")
         status_choices=(('','--select--'),('Active','Active'),('Inactive','Inactive'))
-        print("hksdhfkjsdhf")
+        # print("hksdhfkjsdhf")
         widgets={
             'subcategory_name':forms.TextInput(attrs={'class':'form-control','placeholder':'Subcategory Name'}),
-            'category':forms.Select(attrs={'class':'form-control'},choices=( (x.id, x.category_name) for x in category_choices )),
+            'category':forms.Select(attrs={'class':'form-control'},choices=category_choices),
             'subcategory_des':forms.Textarea(attrs={'class':'form-control','placeholder':'Describe About Subcategory','rows':'4','cols':'5'}),
             'subcategory_status':forms.Select(attrs={'class':'form-control'},choices=status_choices)
         }
