54fe0ad8327dfb8c769aac2fc8da27978507cbde
kopika <taniafci81@gmail.com>
2019-04-20 01:38:43 +0600

    product add,models,product_metarial

--- Raggery/Backend/forms/product_metarial_form.py
+++ Raggery/Backend/forms/product_metarial_form.py
@@ -9,15 +9,14 @@ class ProductMetarialForm(forms.ModelForm):
     class Meta:
         model=ProductMetarial
         category_choices=CategoryModel.objects.filter(category_status='Active')
-        subcategory_choices=SubcategoryModel.objects.filter(subcategory_status='Active')
-        product_type_choices=ProductTypeModel.objects.filter(status='Active')
+   
         status_choices=(('','--select--'),('Active','Active'),('Inactive','Inactive'))
         fields='__all__'
         widgets={
-            'metrial_name':forms.TextInput(attrs={'class':'form-control','placeholder':'Metarial Name'}),
-            'category':forms.Select(attrs={'class':'form-control'},choices=category_choices),
-            'subcategory':forms.Select(attrs={'class':'form-control'},choices=subcategory_choices),
-            'product_type':forms.Select(attrs={'class':'form-control'},choices=product_type_choices),
+            'metarial_name':forms.TextInput(attrs={'class':'form-control','placeholder':'Metarial Name','id':'metarial_name'}),
+            'category':forms.Select(attrs={'class':'form-control','id':'category'}),
+            'subcategory':forms.Select(attrs={'class':'form-control','id':'subcategory'}),
+            'product_type':forms.Select(attrs={'class':'form-control','id':'product_type'}),
             'status':forms.Select(attrs={'class':'form-control'},choices=status_choices)
         }
 
--- Raggery/Backend/models.py
+++ Raggery/Backend/models.py
@@ -12,6 +12,7 @@ class CategoryModel(models.Model):
     def __str__(self):
 
         return self.category_name
+
 class SubcategoryModel(models.Model):
     subcategory_name=models.CharField(max_length=40)
     category=models.ForeignKey(CategoryModel, on_delete=models.DO_NOTHING, default=1)
@@ -21,6 +22,7 @@ class SubcategoryModel(models.Model):
     updated_at=models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.subcategory_name
+
 class ProductTypeModel(models.Model):
     product_type=models.CharField(max_length=40)  
     category=models.ForeignKey(CategoryModel,on_delete=models.DO_NOTHING,default=1)
@@ -29,13 +31,107 @@ class ProductTypeModel(models.Model):
     status=models.CharField(max_length=10)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True) 
+    def __str__(self):
+        return self.pCharFieldroduct_type
+
 class ProductMetarial(models.Model):
-    metrial_name=models.CharField(max_length=50)
+    metarial_name=models.CharField(max_length=50)
     category=models.ForeignKey(CategoryModel,on_delete=models.DO_NOTHING,default=1)
     subcategory=models.ForeignKey(SubcategoryModel,on_delete=models.DO_NOTHING,default=1)
     product_type=models.ForeignKey(ProductTypeModel,on_delete=models.DO_NOTHING,default=1)
     status=models.CharField(max_length=10)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.metarial_name
+
+class BrandModel(models.Model):
+    brand_name=models.CharField(max_length=40)
+    email=models.EmailField(max_length=50)
+    phone=models.CharField(max_length=40)
+    founded_date=models.DateTimeField(auto_now=False, auto_now_add=False)
+    type_of_company=models.CharField(max_length=50)
+    address=models.CharField(max_length=50)
+    description=models.TextField()
+    icon=models.ImageField(upload_to = 'brand_folder/%Y/%m/%d/', default = 'brand_folder/None/no-img.jpg')
+    status=models.CharField(max_length=10)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.brand_name
+
+class ProductSpecificationModel(models.Model):
+    name=models.CharField(max_length=40)
+    category=models.ForeignKey(CategoryModel,on_delete=models.DO_NOTHING,default=1)
+    subcategory=models.ForeignKey(SubcategoryModel,on_delete=models.DO_NOTHING,default=1)
+    product_type=models.ForeignKey(ProductTypeModel,on_delete=models.DO_NOTHING,default=1)
+    status=models.CharField(max_length=10)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+class ProductSizeModel(models.Model):
+    name=models.CharField(max_length=40)
+    category=models.ForeignKey(CategoryModel,on_delete=models.DO_NOTHING,default=1)
+    subcategory=models.ForeignKey(SubcategoryModel,on_delete=models.DO_NOTHING,default=1)
+    product_type=models.ForeignKey(ProductTypeModel,on_delete=models.DO_NOTHING,default=1)
+    status=models.CharField(max_length=10)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+# class TemplateParentModel(models.Model):
+#     product_name=models.CharField(max_length=60)
+#     product_code=models.CharField(max_length=60)
+#     category=models.ForeignKey(CategoryModel,on_delete=models.DO_NOTHING,default=1)
+#     subcategory=models.ForeignKey(SubcategoryModel,on_delete=models.DO_NOTHING,default=1)
+#     product_type=models.ForeignKey(ProductTypeModel,on_delete=models.DO_NOTHING,default=1)
+#     brand=models.ForeignKey(BrandModel,on_delete=models.DO_NOTHING,default=1)
+#     purchase_price=models.CharField(max_length=50)
+#     sale_price=models.CharField(max_length=50)
+#     off_price=models.CharField(max_length=50)
+#     created_at=models.DateTimeField(auto_now_add=True)
+#     updated_at=models.DateTimeField(auto_now=True)
+#     def __str__(self):
+#         return self.product_name
+
+# class TemplateMetarialChildModel(models.Model):
+#     template_parent=models.ForeignKey(TemplateParentModel,on_delete=models.DO_NOTHING,default=1)
+#     metarial_id=models.ForeignKey(ProductMetarial,on_delete=models.DO_NOTHING,default=1)  
+#     created_at=models.DateTimeField(auto_now_add=True)
+#     updated_at=models.DateTimeField(auto_now=True) 
+
+
+# class TemplateSizeChildModel(models.Model):
+#     template_parent=models.ForeignKey(TemplateParentModel,on_delete=models.DO_NOTHING,default=1)
+#     size_id=models.ForeignKey(ProductSizeModel,on_delete=models.DO_NOTHING,default=1)  
+#     created_at=models.DateTimeField(auto_now_add=True)
+#     updated_at=models.DateTimeField(auto_now=True)
+  
+
+# class TemplateSpecificationChild(models.Model):
+#     template_parent=models.ForeignKey(TemplateParentModel,on_delete=models.DO_NOTHING,default=1)
+#     specification_id=models.ForeignKey(ProductSpecificationModel,on_delete=models.DO_NOTHING,default=1)  
+#     created_at=models.DateTimeField(auto_now_add=True)
+#     updated_at=models.DateTimeField(auto_now=True)
+
+# class ProductAddModel(models.Model):
+#     product_code=models.CharField(max_length=50)
+#     template_parent=models.ForeignKey(TemplateParentModel,on_delete=models.DO_NOTHING,default=1)
+#     product_qty=models.IntegerField()
+#     created_at=models.DateTimeField(auto_now_add=True)
+#     updated_at=models.DateTimeField(auto_now=True)
+#     def __str__(self):
+#         return self.product_code
+
+
+
+      
+
+    
+
+
 
 
